4decd9eaf471dd6e3433fccf4860a47e3aa53474
Andrey <andrey.nikitsich@gmail.com>
2021-10-29 14:49:21 +0300

    Do some refactoring.

--- utils/route.py
+++ utils/route.py
@@ -6,6 +6,7 @@ Point_with_action = tuple[Point, list[str]]
 def build_route(plane: Plane, delivery_points_with_actions: list[Point_with_action],
                 start_point: Point = (0, 0)) -> list:
     """Returns a list of commands required to move from the starting point through all delivery points."""
+
     route_points = [(start_point, [])] + delivery_points_with_actions
     commands = []
     for i in range(1, len(route_points)):
@@ -19,11 +20,14 @@ def build_route(plane: Plane, delivery_points_with_actions: list[Point_with_acti
 
 def get_optimal_points_order(point_list: list[Point]) -> list:
     """Returns a sequence of points in the order that provides the shortest path length."""
+
     # we must solve the TSP problem to find shortest path between points.
     # There are so many methods to do this, but implementing one of them is out of scope, I think ;)
+
     return point_list
 
 
 def convert_commands_to_output_format(commands_list: list[str]) -> str:
     """Returns a string with a sequence of commands with the required formatting."""
+
     return ''.join(commands_list)
